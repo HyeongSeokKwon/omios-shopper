597e756ec13aeee4be286c12ce4dc9021aa7f6e2
Hugnry-bird <rnjsgudtjr@naver.com>
2021-08-26 22:21:46 +0900

    auto next  paging

--- lib/page/deepyHome.dart
+++ lib/page/deepyHome.dart
@@ -1,5 +1,7 @@
 import 'package:cloth_collection/widget/image_slide.dart';
+import 'package:cloth_collection/widget/product_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class DeepyHome extends StatefulWidget {
   @override
@@ -15,7 +17,39 @@ class _DeepyHomeState extends State<DeepyHome> {
       child: SingleChildScrollView(
         child: Column(
           children: [
-            ImageSlideHasNum(height * 0.25, width),
+            ImageSlideHasNum(width / 1.618, width), //배너 호출
+            _buildRecommend(), _buildRecommend(), _buildRecommend(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildRecommend() {
+    String recommendText = "Deepy님의 쇼핑몰에 가장 잘 어울리는 옷";
+    return Padding(
+      padding: const EdgeInsets.all(15),
+      child: Container(
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                recommendText,
+                style: TextStyle(fontWeight: FontWeight.w800, fontSize: 17),
+              ),
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ProductCard(),
+                SizedBox(width: 25),
+                ProductCard(),
+                SizedBox(width: 25),
+                ProductCard(),
+              ],
+            ),
           ],
         ),
       ),
