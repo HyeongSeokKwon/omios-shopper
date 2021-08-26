f2bcaeb8cf3def81fe75b092b91c62dd0cc0e232
Hugnry-bird <rnjsgudtjr@naver.com>
2021-08-26 12:42:47 +0900

    Made image slide that has number indicator

--- lib/widget/image_slide.dart
+++ lib/widget/image_slide.dart
@@ -1,16 +1,19 @@
 import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 
-class ImageSlide extends StatefulWidget {
+class ImageSlideHasDot extends StatefulWidget {
   @override
-  _ImageSlideState createState() => _ImageSlideState();
+  _ImageSlideHasDotState createState() => _ImageSlideHasDotState();
 }
 
-class _ImageSlideState extends State<ImageSlide> {
+class _ImageSlideHasDotState extends State<ImageSlideHasDot> {
   PageController _pageController = PageController(initialPage: 0);
   double _currentPosition = 0.0;
   final List<Container> images = <Container>[
-    Container(color: Colors.red, height: 50, width: 20),
+    Container(
+      color: Colors.red,
+      height: 50,
+    ),
     Container(color: Colors.blue, height: 50),
     Container(color: Colors.black, height: 50),
   ];
@@ -18,7 +21,6 @@ class _ImageSlideState extends State<ImageSlide> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 100,
       child: Stack(
         children: [
           PageView.builder(
@@ -42,6 +44,58 @@ class _ImageSlideState extends State<ImageSlide> {
               ),
             ),
           )
+        ],
+      ),
+    );
+  }
+}
+
+class ImageSlideHasNum extends StatefulWidget {
+  final double height;
+  final double width;
+  ImageSlideHasNum(this.height, this.width);
+  @override
+  _ImageSlideHasNumState createState() => _ImageSlideHasNumState();
+}
+
+class _ImageSlideHasNumState extends State<ImageSlideHasNum> {
+  PageController _pageController = PageController(initialPage: 0);
+  double _currentPosition = 0.0;
+  final List<Container> images = <Container>[
+    Container(
+      color: Colors.purple[100],
+      height: 50,
+    ),
+    Container(color: Colors.purple[200], height: 50),
+    Container(color: Colors.purple[300], height: 50),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(
+        children: [
+          PageView.builder(
+            controller: _pageController,
+            itemCount: images.length,
+            itemBuilder: (BuildContext context, int index) {
+              _currentPosition = index.toDouble();
+              return images[index];
+            },
+            onPageChanged: (value) {
+              _currentPosition = value.toDouble();
+              setState(() {});
+            },
+          ),
+          Positioned.fill(
+            child: Align(
+              alignment: Alignment.bottomRight,
+              child: Container(
+                child:
+                    Text("${(_currentPosition + 1).toInt()}/${images.length}"),
+              ),
+            ),
+          ),
         ],
       ),
     );
