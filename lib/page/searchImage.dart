f9ba3592dbd1afaa46445705bd906cda74fb9b01
Hugnry-bird <rnjsgudtjr@naver.com>
2021-08-27 14:28:32 +0900

    add camera and gallery function

--- lib/page/searchImage.dart
+++ lib/page/searchImage.dart
@@ -12,7 +12,6 @@ class _SearchImageState extends State<SearchImage> {
   @override
   Widget build(BuildContext context) {
     final double width = MediaQuery.of(context).size.width;
-
     final double height = MediaQuery.of(context).size.height;
 
     return SingleChildScrollView(
@@ -22,25 +21,51 @@ class _SearchImageState extends State<SearchImage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Center(
-              child: Container(
-                width: width * 0.6,
-                height: width * 0.7,
-                decoration:
-                    BoxDecoration(border: Border.all(color: Colors.grey)),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    _image == null
-                        ? Center(child: Text("찾고싶은 옷이 있으면 올려주세요."))
-                        : Image.file(File(_image!.path)),
-                    SizedBox(height: height * 0.03),
-                    Icon(
-                      Icons.download_outlined,
-                      color: Colors.grey,
-                      size: 35,
-                    ),
-                  ],
+              child: InkWell(
+                child: Container(
+                  width: width * 0.6,
+                  height: width * 0.7,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: Colors.grey),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey,
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      _image == null
+                          ? Center(
+                              child: Column(
+                                children: [
+                                  Text("찾고싶은 옷이 있으면 올려주세요."),
+                                  SizedBox(height: height * 0.03),
+                                  Icon(
+                                    Icons.add_a_photo_outlined,
+                                    color: Colors.grey,
+                                    size: 35,
+                                  ),
+                                ],
+                              ),
+                            )
+                          : Image.file(
+                              File(_image!.path),
+                              width: width * 0.6,
+                              height: width * 0.6,
+                              fit: BoxFit.fitHeight,
+                            ),
+                    ],
+                  ),
                 ),
+                onTap: () {
+                  showPicker(context);
+                },
               ),
             ),
           ],
@@ -49,11 +74,58 @@ class _SearchImageState extends State<SearchImage> {
     );
   }
 
-  Future getImageFromGallery() async {
-    var image =
-        await ImagePicker.platform.pickImage(source: ImageSource.gallery);
+  Future getImageFromPhoto() async {
+    // ignore: invalid_use_of_visible_for_testing_member
+    var image = await ImagePicker.platform
+        .pickImage(source: ImageSource.camera, imageQuality: 50);
+
     setState(() {
       _image = image;
     });
+  }
+
+  Future getImageFromGallery() async {
+    var image =
+// ignore: invalid_use_of_visible_for_testing_member
+        await ImagePicker.platform.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      _image = image;
+    });
+  }
+
+  void showPicker(context) {
+    showModalBottomSheet(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(10),
+          topRight: Radius.circular(10),
+        )),
+        context: context,
+        builder: (BuildContext bc) {
+          return SafeArea(
+            child: Container(
+              child: Wrap(
+                children: <Widget>[
+                  ListTile(
+                      leading: Icon(Icons.photo_library),
+                      title: Text('Photo Library'),
+                      onTap: () {
+                        getImageFromGallery();
+                        Navigator.of(context).pop();
+                      }),
+                  ListTile(
+                    leading: Icon(Icons.photo_camera),
+                    title: Text('Camera'),
+                    onTap: () {
+                      getImageFromPhoto();
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
   }
 }
