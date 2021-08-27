3ea4c378a23a02a3bd18f4f02513398ee60b58b9
Hugnry-bird <rnjsgudtjr@naver.com>
2021-08-27 12:29:21 +0900

    create searchImage widget

--- lib/page/searchImage.dart
+++ lib/page/searchImage.dart
@@ -1,6 +1,4 @@
 import 'dart:io';
-
-import 'package:cloth_collection/widget/product_card.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -13,16 +11,40 @@ class _SearchImageState extends State<SearchImage> {
   PickedFile? _image;
   @override
   Widget build(BuildContext context) {
+    final double width = MediaQuery.of(context).size.width;
+
+    final double height = MediaQuery.of(context).size.height;
+
     return SingleChildScrollView(
-      child: Column(
-        children: [
-          Container(
-            child: _image == null
-                ? Text("선택된 이미지가 없습니다.")
-                : Image.file(File(_image!.path)),
-          ),
-          FloatingActionButton(onPressed: getImageFromGallery)
-        ],
+      child: Padding(
+        padding: const EdgeInsets.only(top: 130),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Container(
+                width: width * 0.6,
+                height: width * 0.7,
+                decoration:
+                    BoxDecoration(border: Border.all(color: Colors.grey)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    _image == null
+                        ? Center(child: Text("찾고싶은 옷이 있으면 올려주세요."))
+                        : Image.file(File(_image!.path)),
+                    SizedBox(height: height * 0.03),
+                    Icon(
+                      Icons.download_outlined,
+                      color: Colors.grey,
+                      size: 35,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
