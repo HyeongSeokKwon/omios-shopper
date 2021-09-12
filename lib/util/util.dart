63ce1b1ac3fc29dd13f53535b62e970f00219877
Hugnry-bird <rnjsgudtjr@naver.com>
2021-09-12 17:58:53 +0900

    create textstyle form

--- lib/util/util.dart
+++ lib/util/util.dart
@@ -1,3 +1,14 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
 const MAINCOLOR = Color.fromRGBO(176, 140, 217, 100);
+
+TextStyle textstyle(
+    Color color, FontWeight fontWeight, var fontFamily, var fontSize) {
+  return TextStyle(
+      color: color,
+      fontWeight: fontWeight,
+      fontFamily: fontFamily,
+      fontSize: fontSize);
+}
