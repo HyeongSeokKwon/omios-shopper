7e3c0bd7683cefa6756a9ca32aa2a8d239f9a58c
Hugnry-bird <rnjsgudtjr@naver.com>
2021-09-18 12:57:00 +0900

    apply icon of bottomNavigation Bar

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -2,9 +2,13 @@ import 'package:cloth_collection/page/SearchImage.dart';
 import 'package:cloth_collection/page/deepyHome/deepyHome.dart';
 import 'package:cloth_collection/page/chatting.dart';
 import 'package:cloth_collection/page/myPage.dart';
-import 'package:cloth_collection/util/util.dart';
 import 'package:cloth_collection/widget/main_Appbar.dart';
 import 'package:flutter/material.dart';
+
+const int HOME = 0;
+const int SEARCH = 1;
+const int CHAT = 2;
+const int MYPAGE = 3;
 
 class HomePage extends StatefulWidget {
   @override
@@ -14,16 +18,55 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   int _currentIndex = 0;
   FocusNode focusNode = FocusNode();
-  List<Widget> selectedPage = [
-    DeepyHome(),
-    SearchImage(),
-    Chatting(),
-    MyPage(),
+  ScrollController scrollController = ScrollController();
+
+  late List<Widget> selectedPage;
+  List<String> currentIconUrl = [
+    "assets/images/homeTapped.png",
+    "assets/images/searchImage.png",
+    "assets/images/chat.png",
+    "assets/images/myPage.png"
   ];
+  List<String> navigationIconUrl = [
+    "assets/images/home.png",
+    "assets/images/searchImage.png",
+    "assets/images/chat.png",
+    "assets/images/myPage.png"
+  ];
+  List<String> navigationOnTapIconUrl = [
+    "assets/images/homeTapped.png",
+    "assets/images/searchImageTapped.png",
+    "assets/images/chatTapped.png",
+    "assets/images/myPageTapped.png"
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    selectedPage = [
+      DeepyHome(scrollController),
+      SearchImage(),
+      Chatting(),
+      MyPage(),
+    ];
+  }
+
+  void _onItemTapped(int index) {
+    if (_currentIndex != index) {
+      setState(() {
+        currentIconUrl[_currentIndex] = navigationIconUrl[_currentIndex];
+        _currentIndex = index;
+        currentIconUrl[_currentIndex] = navigationOnTapIconUrl[_currentIndex];
+      });
+    }
+    if (index == HOME) {
+      scrollController.animateTo(0.0,
+          duration: Duration(milliseconds: 300), curve: Curves.easeIn);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    final width = MediaQuery.of(context).size.width;
-    final height = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: MainAppbar(focusNode),
       body: selectedPage[_currentIndex],
@@ -31,48 +74,28 @@ class _HomePageState extends State<HomePage> {
         items: [
           BottomNavigationBarItem(
             label: "홈",
-            icon: Icon(
-              Icons.home,
-              color: Colors.purple[100],
-            ),
+            icon: Image.asset(currentIconUrl[HOME]),
           ),
           BottomNavigationBarItem(
             label: "이미지검색",
-            icon: Icon(
-              Icons.home,
-              color: Colors.purple[100],
-            ),
+            icon: Image.asset(currentIconUrl[SEARCH]),
           ),
           BottomNavigationBarItem(
             label: "채팅",
-            icon: Icon(
-              Icons.home,
-              color: Colors.purple[100],
-            ),
+            icon: Image.asset(currentIconUrl[CHAT]),
           ),
           BottomNavigationBarItem(
             label: "마이페이지",
-            icon: Icon(
-              Icons.home,
-              color: Colors.purple[100],
-            ),
+            icon: Image.asset(currentIconUrl[MYPAGE]),
           ),
         ],
-        selectedItemColor: MAINCOLOR,
-        unselectedItemColor: Colors.grey,
-        selectedIconTheme: IconThemeData(color: MAINCOLOR),
-        unselectedIconTheme: IconThemeData(color: Colors.grey),
         showUnselectedLabels: true,
         selectedFontSize: 12.0,
-        onTap: onItemTapped,
+        selectedItemColor: const Color(0xffec5363),
+        onTap: _onItemTapped,
         currentIndex: _currentIndex,
         type: BottomNavigationBarType.fixed,
       ),
     );
-  }
-
-  void onItemTapped(int index) {
-    _currentIndex = index;
-    setState(() {});
   }
 }
