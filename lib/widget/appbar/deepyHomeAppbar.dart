daefa3c1401a94236ecc022be59b7da6774366d3
Hugnry-bird <rnjsgudtjr@naver.com>
2021-10-06 22:07:39 +0900

    modified appbar's leading area

--- lib/widget/appbar/deepyHomeAppbar.dart
+++ lib/widget/appbar/deepyHomeAppbar.dart
@@ -12,12 +12,20 @@ class DeepyHomeAppbar extends StatelessWidget with PreferredSizeWidget {
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     return AppBar(
-      automaticallyImplyLeading: false,
+      automaticallyImplyLeading: true,
       elevation: 0,
-      title: Container(
-        child: Text("Deepy",
-            style: textStyle(
-                const Color(0xff333333), FontWeight.w700, "NotoSansKR", 20.0)),
+      leadingWidth: width * 0.3,
+      leading: Container(
+        child: Center(
+          child: Row(
+            children: [
+              SizedBox(width: width * 0.053),
+              Text("Deepy",
+                  style: textStyle(const Color(0xff333333), FontWeight.w700,
+                      "NotoSansKR", 22.0)),
+            ],
+          ),
+        ),
       ),
       actions: <Widget>[
         Padding(
--- lib/widget/appbar/myPage_Appbar.dart
+++ lib/widget/appbar/myPage_Appbar.dart
@@ -6,13 +6,22 @@ class MypageAppbar extends StatelessWidget with PreferredSizeWidget {
   Size get preferredSize => Size.fromHeight(kToolbarHeight);
   @override
   Widget build(BuildContext context) {
+    final double width = MediaQuery.of(context).size.width;
     return AppBar(
       automaticallyImplyLeading: false,
       elevation: 0,
-      title: Container(
-        child: Text("마이페이지",
-            style: textStyle(
-                const Color(0xff333333), FontWeight.w700, "NotoSansKR", 20.0)),
+      leadingWidth: width * 0.3,
+      leading: Container(
+        child: Center(
+          child: Row(
+            children: [
+              SizedBox(width: width * 0.053),
+              Text("마이페이지",
+                  style: textStyle(const Color(0xff333333), FontWeight.w700,
+                      "NotoSansKR", 22.0)),
+            ],
+          ),
+        ),
       ),
     );
   }
