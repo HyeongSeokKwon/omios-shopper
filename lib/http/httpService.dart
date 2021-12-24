12aacd94fc90e57f8878f542dea4374297e1a563
Hugnry-bird <rnjsgudtjr@naver.com>
2021-12-24 17:53:42 +0900

    static => 객체 선언

--- lib/http/httpService.dart
+++ lib/http/httpService.dart
@@ -1,43 +1,66 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:cloth_collection/model/loginModel.dart';
+import 'package:get/get_connect/http/src/exceptions/exceptions.dart';
 import 'package:http/http.dart' as http;
 import 'package:jwt_decode/jwt_decode.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import 'httpException.dart';
 
 // 13.209.244.41/user/token/            : id, pwd 이용해서 token 발급
 // 13.209.244.41/user/token/refresh/    : refresh token이용해서 token 발급
 // username = rkdeowls
 // password = rkdeowls
+
+// 1. access token 만료 확인
+// 2. refresh token 만료 확인
+// 3-1. refresh token이 만료가 되었으면, 로그아웃
+// 3-2. refresh token이 만료가 안되었으면, refresh token 이용해서 token 발급
 class HttpService {
   static var refreshToken;
   static var accessToken;
-  static var baseUrl = 'http://13.209.244.41';
-  static late SharedPreferences pref;
+  var baseUrl = 'http://13.209.244.41';
+  late SharedPreferences pref;
 
-  static void getToken() async {
+  dynamic _response(http.Response response) {
+    switch (response.statusCode) {
+      case 200:
+        var responseJson = json.decode(response.body);
+        return responseJson;
+      case 401:
+        var responseJson = json.decode(response.body);
+        return responseJson;
+      case 400:
+        throw BadRequestException(response.body.toString());
+      case 403:
+        throw UnauthorisedException(response.body.toString());
+      case 500:
+      default:
+        throw FetchDataException(
+            'Error occured while Communication with Server with StatusCode:${response.statusCode}');
+    }
+  }
+
+  void getToken() async {
     pref = await SharedPreferences.getInstance();
     refreshToken = pref.getString('refreshToken');
     accessToken = pref.getString('accessToken');
   }
 
-  static void setRefreshToken(var changedRefreshToken) async {
+  void setRefreshToken(var changedRefreshToken) async {
     pref = await SharedPreferences.getInstance();
     pref.setString('refreshToken', changedRefreshToken);
     refreshToken = changedRefreshToken;
   }
 
-  static void setAccessToken(var changedAccessToken) async {
+  void setAccessToken(var changedAccessToken) async {
     pref = await SharedPreferences.getInstance();
     pref.setString('accessToken', changedAccessToken);
     accessToken = changedAccessToken;
   }
 
-  static bool isAccessExpired() {
-    // 1. access token 만료 확인
-    // 2. refresh token 만료 확인
-    // 3-1. refresh token이 만료가 되었으면, 로그아웃
-    // 3-2. refresh token이 만료가 안되었으면, refresh token 이용해서 token 발급
+  bool isAccessExpired() {
     if (accessToken == null || Jwt.isExpired(accessToken)) {
       return true;
     } else {
@@ -45,7 +68,7 @@ class HttpService {
     }
   }
 
-  static bool isRefreshExpired() {
+  bool isRefreshExpired() {
     if (refreshToken == null || Jwt.isExpired(refreshToken)) {
       return true;
     } else {
@@ -53,65 +76,64 @@ class HttpService {
     }
   }
 
-  static void updateToken() async {
+  void updateToken() async {
     // refresh token으로 accessToken 갱신시키는 함수
-    var decodedValidationResponse;
+    var responseJson;
     if (isAccessExpired()) {
       //access token 만료 되었으면
       if (!isRefreshExpired()) {
         // refresh token 만료 되지 않았으면
-        var validationResponse = await http.post(
+        var response = await http.post(
           Uri.parse(baseUrl + '/user/token/refresh/'), // refresh token 으로 재발급
           headers: {"Content-Type": "application/json; charset=UTF-8"},
           body: json.encode(
             {"refresh": refreshToken},
           ),
         );
-        print(validationResponse.body);
-        decodedValidationResponse = jsonDecode(validationResponse.body);
-        setAccessToken(decodedValidationResponse['access']);
-        setRefreshToken(decodedValidationResponse['refresh']);
+        responseJson = _response(response);
+
+        setAccessToken(responseJson['access']);
+        setRefreshToken(responseJson['refresh']);
       }
     }
   }
 
-  static Future<dynamic> httpGet(String additionalUrl) async {
+  Future<dynamic> httpGet(String additionalUrl) async {
     var response;
     var responseBody;
+    var responseJson;
     updateToken();
 
     response = await http.get(Uri.parse(baseUrl + additionalUrl),
         headers: {HttpHeaders.authorizationHeader: 'Bearer $accessToken'});
-    print("get response>>>>>>>>>>>>>>>>>>>");
-    var a = jsonDecode(response.body);
-    print(a['message']);
-    print(a['message'].runtimeType);
     responseBody = utf8.decode(response.bodyBytes);
-    print(jsonDecode(responseBody));
-    // print(jsonDecode(responseBody).runtimeType);
-    return jsonDecode(responseBody);
+    responseJson = _response(responseBody);
+    return responseJson;
   }
 
-  static Future<dynamic> httpPost(String addtionalUrl, var body) async {
+  Future<dynamic> httpPost(String addtionalUrl, var body) async {
     var response;
+    var responseJson;
     if (addtionalUrl != "/user/token/") {
-      //ID PW 로 갱신 시키는 요청은 굳이 updateToken 과정 필요없음
       updateToken();
     }
 
     response = await http.post(Uri.parse(baseUrl + addtionalUrl),
         headers: {HttpHeaders.authorizationHeader: 'Bearer $accessToken'},
         body: body);
+    try {
+      responseJson = _response(response);
+      return responseJson;
+    } on SocketException {
+      throw FetchDataException('No internet Connection');
+    }
 
-    print("post response>>>>>>>>>>>");
-    print(response.body);
-    print(response.body.runtimeType);
-    return response;
+    // return response;
   }
 
-  static Future<dynamic> httpPatch(String addtionalUrl) async {}
+  Future<dynamic> httpPatch(String addtionalUrl) async {}
 
-  static Future<dynamic> httpDelete(String addtionalUrl) async {}
+  Future<dynamic> httpDelete(String addtionalUrl) async {}
 }
 
 class LoginHttp {
