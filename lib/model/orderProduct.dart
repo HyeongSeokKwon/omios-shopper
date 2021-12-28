4aad113f7b895716cab7539d916ede992a3e50d3
Hugnry-bird <rnjsgudtjr@naver.com>
2021-12-28 21:23:42 +0900

    주문 객체 생성

---
+++ lib/model/orderProduct.dart
@@ -0,0 +1,12 @@
+class OrderProduct {
+  String color;
+  String size;
+  int count;
+  int price;
+  OrderProduct({
+    required this.color,
+    required this.size,
+    required this.count,
+    required this.price,
+  });
+}
