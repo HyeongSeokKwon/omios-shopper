1038b5eea21775cc0c48d07549661e0d8a2cdbfd
Hugnry-bird <rnjsgudtjr@naver.com>
2022-01-11 16:00:31 +0900

    category api 호출

--- lib/controller/categoryController.dart
+++ lib/controller/categoryController.dart
@@ -1,5 +1,12 @@
+import 'package:cloth_collection/http/httpService.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+class Category {
+  int id;
+  String name;
+  Category({required this.id, required this.name});
+}
 
 class CategoryController extends GetxController {
   double startPrice = 1.0;
@@ -7,6 +14,34 @@ class CategoryController extends GetxController {
   RangeValues priceRange = RangeValues(0.0, 100000.0);
   List<int> selectedColor = [];
   int optionMode = 0;
+  Category mainCategory = Category(id: 1, name: "init");
+
+  HttpService httpService = HttpService();
+  late Future<List<Category>> categoryList;
+
+  Future<List<dynamic>> getCategory() async {
+    var response = await httpService.httpGet("/category/");
+    return response['data'];
+  }
+
+  Future<List<dynamic>> getSubCategory(int mainCategoryId) async {
+    var response =
+        await httpService.httpGet("/category/$mainCategoryId/subcategory/");
+    print("getsubCategory");
+    print(response['data']);
+
+    var responses = await httpService.httpGet("/product/");
+    print("mainCategory");
+    print(responses);
+    // print(responses);
+
+    return response['data']['subcategory'];
+  }
+
+  void selectMainCategory(Map<String, dynamic> mainCategoryInfo) {
+    mainCategory.id = mainCategoryInfo['id'];
+    mainCategory.name = mainCategoryInfo['name'];
+  }
 
   void refreshColorOption() {
     selectedColor = [];
