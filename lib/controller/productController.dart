1ac86b0b8f58fe362155aedf5960d4d9f57fd740
Hugnry-bird <rnjsgudtjr@naver.com>
2022-01-16 18:02:01 +0900

    queryParameter add 방식 변경, option controller 위치 변경

--- lib/controller/productController.dart
+++ lib/controller/productController.dart
@@ -1,5 +1,13 @@
 import 'package:cloth_collection/http/httpService.dart';
+import 'package:flutter/material.dart';
 import 'package:get/state_manager.dart';
+
+class SearchOption {
+  List<int> color;
+  RangeValues priceRange;
+
+  SearchOption({required this.color, required this.priceRange});
+}
 
 class ProductController extends GetxController {
   HttpService httpService = HttpService();
@@ -8,42 +16,58 @@ class ProductController extends GetxController {
   String? prevDataLink = "";
   String? nextDataLink = "";
 
-  bool isLoading = false;
+  double startPrice = 1.0;
+  double endPrice = 99999.0;
+  RangeValues priceRange = RangeValues(0.0, 100000.0);
+  List<int> selectedColor = [];
+  int optionMode = 0;
+  late SearchOption searchOption;
+  late int subCategoryId;
 
-  void initGetProducts(int mainCategoryId, int subCategoryId) async {
+  bool isLoading = false;
+  Map<String, String> queryParams = {};
+
+  Future<void> initGetProducts(int mainCategoryId, int subCategoryID) async {
     var response;
     isLoading = true;
-    if (subCategoryId == 0) {
-      response =
-          await httpService.httpGet("/product/?main_category=$mainCategoryId");
-    } else {
-      response = await httpService.httpGet(
-          "/product/?main_category=$mainCategoryId&sub_category=$subCategoryId");
+    subCategoryId = subCategoryID;
+
+    queryParams['main_category'] = '$mainCategoryId';
+    if (subCategoryId != 0) {
+      queryParams['sub_category'] = '$subCategoryId';
     }
+    response = await httpService.httpGet("product", queryParams);
     productData = response["data"]["results"];
     prevDataLink = response["data"]["previous"];
     nextDataLink = response["data"]["next"];
     isLoading = false;
+
+    print(productData);
     update();
   }
 
   void getMainCategoryProducts(int mainCategoryId) {
-    String url = "/product/?main_category=$mainCategoryId";
-    getProducts(url);
+    queryParams = {};
+    queryParams['main_category'] = '$mainCategoryId';
+
+    getProducts();
   }
 
   void getSubCategoryProducts(int mainCategoryId, int subCategoryId) {
-    String url =
-        "/product/?main_category=$mainCategoryId&sub_category=$subCategoryId";
-    getProducts(url);
+    queryParams = {};
+    queryParams['main_category'] = '$mainCategoryId';
+    queryParams['sub_category'] = '$subCategoryId';
+
+    getProducts();
   }
 
-  void getProducts(String url) async {
+  void getProducts() async {
     var response;
     isLoading = true;
     if (nextDataLink != null) {
-      response = await httpService.httpGet(nextDataLink!.substring(12));
-      print(response['data']);
+      queryParams['page'] = nextDataLink!.substring(nextDataLink!.length - 1);
+      response = await httpService.httpGet("product", queryParams);
+      print(response);
     } else {
       return;
     }
@@ -53,6 +77,63 @@ class ProductController extends GetxController {
     productData = productData + response['data']['results'];
 
     isLoading = false;
+    update();
+  }
+
+  void refreshColorOption() {
+    selectedColor = [];
+    update();
+  }
+
+  void refreshPriceOption() {
+    priceRange = RangeValues(0.0, 100000.0);
+    update(["priceOption"]);
+  }
+
+  void priceRangeChange(RangeValues changeRange) {
+    priceRange = changeRange;
+    update(["priceOption"]);
+  }
+
+  bool isColorSelected(int colorIndex) {
+    if (selectedColor.contains(colorIndex)) {
+      return true;
+    }
+    return false;
+  }
+
+  void selectColor(int colorIndex) {
+    if (selectedColor.contains(colorIndex)) {
+      selectedColor.remove(colorIndex);
+      update();
+    } else {
+      selectedColor.add(colorIndex);
+      update();
+    }
+    print(selectedColor);
+  }
+
+  void searchClicked(int mainCategoryId) async {
+    var response;
+    searchOption = SearchOption(color: selectedColor, priceRange: priceRange);
+    int startPrice = searchOption.priceRange.start.toInt();
+    int endPrice = searchOption.priceRange.end.toInt();
+    productData = [];
+    queryParams = {};
+
+    queryParams['main_category'] = '$mainCategoryId';
+    queryParams['minprice'] = '$startPrice';
+    queryParams['maxprice'] = '$endPrice';
+
+    if (subCategoryId != 0) {
+      queryParams['sub_category'] = '$subCategoryId';
+    }
+
+    response = await httpService.httpGet("product", queryParams);
+    print(response);
+    prevDataLink = response['data']['previous'];
+    nextDataLink = response['data']['next'];
+    productData = productData + response['data']['results'];
     update();
   }
 }
