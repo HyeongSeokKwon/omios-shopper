25165e133d736a088245d5474b95dfd5428c69ce
Hugnry-bird <rnjsgudtjr@naver.com>
2022-01-16 18:03:17 +0900

    product option controller 위치옮김

--- lib/controller/categoryController.dart
+++ lib/controller/categoryController.dart
@@ -1,5 +1,4 @@
 import 'package:cloth_collection/http/httpService.dart';
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class Category {
@@ -9,45 +8,27 @@ class Category {
 }
 
 class CategoryController extends GetxController {
-  double startPrice = 1.0;
-  double endPrice = 99999.0;
-  RangeValues priceRange = RangeValues(0.0, 100000.0);
-  List<int> selectedColor = [];
-  int optionMode = 0;
   Category mainCategory = Category(id: 1, name: "init");
 
   HttpService httpService = HttpService();
   late Future<List<Category>> categoryList;
+  Map<String, String> queryParams = {};
 
   Future<List<dynamic>> getCategory() async {
-    var response = await httpService.httpGet("/product/main-category/");
+    var response = await httpService.httpGet("product/main-category");
     print(response);
     return response['data'];
   }
 
   Future<List<dynamic>> getSubCategory(int mainCategoryId) async {
     var response = await httpService
-        .httpGet("/product/main-category/$mainCategoryId/sub-category/");
+        .httpGet("product/main-category/$mainCategoryId/sub-category");
 
     return response['data']['sub_category'];
   }
 
-  Future<List<dynamic>> getAllProduct() async {
-    var response =
-        await httpService.httpGet("/product/?main_category=${mainCategory.id}");
-
-    return response['data']['results'];
-  }
-
-  Future<List<dynamic>> getSubCategoryProduct(int subCategoryId) async {
-    var response =
-        await httpService.httpGet("/product/?sub_category=$subCategoryId");
-
-    return response['data']['results'];
-  }
-
   Future<List<dynamic>> getColorImage() async {
-    var response = await httpService.httpGet("/product/color/");
+    var response = await httpService.httpGet("product/color");
 
     return response['data'];
   }
@@ -55,38 +36,5 @@ class CategoryController extends GetxController {
   void selectMainCategory(Map<String, dynamic> mainCategoryInfo) {
     mainCategory.id = mainCategoryInfo['id'];
     mainCategory.name = mainCategoryInfo['name'];
-  }
-
-  void refreshColorOption() {
-    selectedColor = [];
-    update();
-  }
-
-  void refreshPriceOption() {
-    priceRange = RangeValues(0.0, 100000.0);
-    update();
-  }
-
-  void priceRangeChange(RangeValues changeRange) {
-    priceRange = changeRange;
-    update();
-  }
-
-  bool isColorSelected(int colorIndex) {
-    if (selectedColor.contains(colorIndex)) {
-      return true;
-    }
-    return false;
-  }
-
-  void selectColor(int colorIndex) {
-    if (selectedColor.contains(colorIndex)) {
-      selectedColor.remove(colorIndex);
-      update();
-    } else {
-      selectedColor.add(colorIndex);
-      update();
-    }
-    print(selectedColor);
   }
 }
