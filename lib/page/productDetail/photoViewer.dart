804e91f409b28d011ce8fbbdd824e61837c6f3a0
Hugnry-bird <rnjsgudtjr@naver.com>
2022-01-25 22:10:24 +0900

    ios 에서 page bounce 효과 방지(photo_view 한정)

--- lib/page/productDetail/photoViewer.dart
+++ lib/page/productDetail/photoViewer.dart
@@ -14,65 +14,73 @@ class PhotoViewer extends StatefulWidget {
 
 class _PhotoViewerState extends State<PhotoViewer> {
   late int curIndex;
+  late PageController _pageController;
   @override
   void initState() {
-    curIndex = widget.index;
     super.initState();
+    curIndex = widget.index;
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    PageController _pageController = PageController(initialPage: widget.index);
+    _pageController = PageController(initialPage: widget.index);
     return SafeArea(
       top: true,
       child: Scaffold(
-        body: Container(
-          child: Stack(
-            children: [
-              PageView.builder(
-                controller: _pageController,
-                itemCount: widget.imageList.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                      child: PhotoView(
-                    minScale: PhotoViewComputedScale.contained,
-                    imageProvider: CachedNetworkImageProvider(
-                        "${widget.imageList[index]['url']}"),
-                  ));
+        body: Stack(
+          children: [
+            PageView.builder(
+              allowImplicitScrolling: true,
+              controller: _pageController,
+              physics: ClampingScrollPhysics(),
+              itemCount: widget.imageList.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                    child: PhotoView(
+                  minScale: PhotoViewComputedScale.contained,
+                  imageProvider: CachedNetworkImageProvider(
+                      "${widget.imageList[index]['url']}"),
+                ));
+              },
+              onPageChanged: (value) {
+                curIndex = value;
+
+                setState(() {});
+              },
+            ),
+            Positioned(
+              top: 20 * Scale.height,
+              right: 20 * Scale.height,
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.pop(context);
                 },
-                onPageChanged: (value) {
-                  curIndex = value;
-                  setState(() {});
-                },
+                child: Icon(
+                  Icons.clear,
+                  color: Colors.white,
+                ),
               ),
-              Positioned(
-                top: 20 * Scale.height,
-                right: 20 * Scale.height,
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: Icon(
-                    Icons.clear,
-                    color: Colors.white,
+            ),
+            Positioned(
+              top: 20 * Scale.height,
+              child: Container(
+                width: 414 * Scale.width,
+                child: Center(
+                  child: Text(
+                    "${(curIndex + 1).toInt()} / ${widget.imageList.length}",
+                    style: textStyle(const Color(0xffffffff), FontWeight.w700,
+                        "NotoSansKR", 20.0),
                   ),
                 ),
               ),
-              Positioned(
-                top: 20 * Scale.height,
-                child: Container(
-                  width: 414 * Scale.width,
-                  child: Center(
-                    child: Text(
-                      "${(curIndex + 1).toInt()} / ${widget.imageList.length}",
-                      style: textStyle(const Color(0xffffffff), FontWeight.w700,
-                          "NotoSansKR", 20.0),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
