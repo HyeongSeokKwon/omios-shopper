7e9c40747fc0acb105b7aedc959dbabe6b0f71d9
kwonhyeongseok <rnjsgudtjr@naver.com>
2022-06-14 16:11:50 +0900

    home page bottom navigation bar tab 이벤트시 setState 방식으로 변경

--- lib/controller/homeController.dart
+++ lib/controller/homeController.dart
@@ -8,38 +8,8 @@ class HomeController extends GetxController {
   final int mypage = 3;
 
   HttpRepository httpRepository = HttpRepository();
-  List<String> currentIconUrl = [
-    "assets/images/svg/homeTapped.svg",
-    "assets/images/svg/category.svg",
-    "assets/images/svg/bottomNavigationUnlike.svg",
-    "assets/images/svg/feed.svg",
-    "assets/images/svg/myPage.svg",
-  ];
-  List<String> navigationIconUrl = [
-    "assets/images/svg/home.svg",
-    "assets/images/svg/category.svg",
-    "assets/images/svg/bottomNavigationUnlike.svg",
-    "assets/images/svg/feed.svg",
-    "assets/images/svg/myPage.svg"
-  ];
-  List<String> navigationOnTapIconUrl = [
-    "assets/images/svg/homeTapped.svg",
-    "assets/images/svg/categoryTapped.svg",
-    "assets/images/svg/bottomNavigationLike.svg",
-    "assets/images/svg/feedTapped.svg",
-    "assets/images/svg/myPageTapped.svg"
-  ];
 
   int currentIndex = 0;
-
-  void onItemTapped(int index) {
-    if (currentIndex != index) {
-      currentIconUrl[currentIndex] = navigationIconUrl[currentIndex];
-      currentIndex = index;
-      currentIconUrl[currentIndex] = navigationOnTapIconUrl[currentIndex];
-    }
-    update();
-  }
 
   Future<dynamic> getTodaysProducts() async {
     Map<String, String> queryParams = {};
--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,4 +1,3 @@
-import 'package:cloth_collection/controller/homeController.dart';
 import 'package:cloth_collection/database/db.dart';
 import 'package:cloth_collection/page/category/category.dart';
 import 'package:cloth_collection/page/deepyHome/deepyHome.dart';
@@ -12,7 +11,6 @@ import 'package:cloth_collection/widget/appbar/imageSearch_Appbar.dart';
 import 'package:cloth_collection/widget/appbar/myPage_Appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:get/get.dart';
 
 import 'chatting/chatting.dart';
 
@@ -30,10 +28,14 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage>
     with AutomaticKeepAliveClientMixin {
   int _currentIndex = 0;
-  HomeController homeController = HomeController();
   FocusNode focusNode = FocusNode();
-  late List<Widget> selectedPage;
-
+  List<Widget> selectedPage = [
+    DeepyHome(),
+    Category(),
+    LikePage(),
+    Chatting(),
+    MyPage(),
+  ];
   List<PreferredSizeWidget> appBar = [
     DeepyHomeAppbar(),
     CategoryAppbar(),
@@ -41,24 +43,33 @@ class _HomePageState extends State<HomePage>
     ImageSearchAppbar(),
     MypageAppbar(),
   ];
+  List<String> currentIconUrl = [
+    "assets/images/svg/homeTapped.svg",
+    "assets/images/svg/category.svg",
+    "assets/images/svg/bottomNavigationUnlike.svg",
+    "assets/images/svg/feed.svg",
+    "assets/images/svg/myPage.svg",
+  ];
+  List<String> navigationIconUrl = [
+    "assets/images/svg/home.svg",
+    "assets/images/svg/category.svg",
+    "assets/images/svg/bottomNavigationUnlike.svg",
+    "assets/images/svg/feed.svg",
+    "assets/images/svg/myPage.svg"
+  ];
+  List<String> navigationOnTapIconUrl = [
+    "assets/images/svg/homeTapped.svg",
+    "assets/images/svg/categoryTapped.svg",
+    "assets/images/svg/bottomNavigationLike.svg",
+    "assets/images/svg/feedTapped.svg",
+    "assets/images/svg/myPageTapped.svg"
+  ];
 
   @override
   void initState() {
     super.initState();
+    print('init');
     DBHelper().db;
-    selectedPage = [
-      DeepyHome(),
-      Category(),
-      LikePage(),
-      Chatting(),
-      MyPage(),
-    ];
-  }
-
-  @override
-  void dispose() {
-    homeController.dispose();
-    super.dispose();
   }
 
   @override
@@ -66,19 +77,9 @@ class _HomePageState extends State<HomePage>
     super.build(context);
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(70),
-        child: GetBuilder<HomeController>(
-            init: homeController,
-            builder: (controller) {
-              return appBar[homeController.currentIndex];
-            }),
-      ),
+          preferredSize: Size.fromHeight(70), child: appBar[_currentIndex]),
       backgroundColor: Colors.white,
-      body: GetBuilder<HomeController>(
-          init: homeController,
-          builder: (controller) {
-            return selectedPage[homeController.currentIndex];
-          }),
+      body: selectedPage[_currentIndex],
       bottomNavigationBar: _buildBottomNaviagationBar(),
     );
   }
@@ -89,112 +90,113 @@ class _HomePageState extends State<HomePage>
         topLeft: Radius.circular(30.0),
         topRight: Radius.circular(30.0),
       ),
-      child: GetBuilder<HomeController>(
-          init: homeController,
-          builder: (controller) {
-            return BottomNavigationBar(
-              backgroundColor: const Color(0xffffffff),
-              selectedLabelStyle: TextStyle(fontSize: 0),
-              unselectedLabelStyle: TextStyle(fontSize: 0),
-              items: [
-                BottomNavigationBarItem(
-                  label: "",
-                  icon: Padding(
-                    padding: EdgeInsets.only(top: 5 * Scale.height),
-                    child: Column(
-                      children: [
-                        SvgPicture.asset(controller.currentIconUrl[HOME]),
-                        SizedBox(height: 6 * Scale.height),
-                        Text(
-                          "홈",
-                          style: textStyle(const Color(0xffcccccc),
-                              FontWeight.w400, "NotoSansKR", 12.0),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-                BottomNavigationBarItem(
-                  label: "",
-                  icon: Padding(
-                    padding: EdgeInsets.only(top: 5 * Scale.height),
-                    child: Column(
-                      children: [
-                        SvgPicture.asset(controller.currentIconUrl[CATEGORY]),
-                        SizedBox(height: 6 * Scale.height),
-                        Text(
-                          "카테고리",
-                          style: textStyle(const Color(0xffcccccc),
-                              FontWeight.w400, "NotoSansKR", 12.0),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-                BottomNavigationBarItem(
-                  label: "",
-                  icon: Padding(
-                    padding: EdgeInsets.only(top: 5 * Scale.height),
-                    child: Column(
-                      children: [
-                        SvgPicture.asset(controller.currentIconUrl[LIKE]),
-                        SizedBox(height: 6 * Scale.height),
-                        Text(
-                          "찜한 상품",
-                          style: textStyle(const Color(0xffcccccc),
-                              FontWeight.w400, "NotoSansKR", 12.0),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-                BottomNavigationBarItem(
-                  label: "",
-                  icon: Padding(
-                    padding: EdgeInsets.only(
-                      top: 5 * Scale.height,
-                    ),
-                    child: Column(
-                      children: [
-                        SvgPicture.asset(controller.currentIconUrl[CHAT]),
-                        SizedBox(height: 6 * Scale.height),
-                        Text(
-                          "피드",
-                          style: textStyle(const Color(0xffcccccc),
-                              FontWeight.w400, "NotoSansKR", 12.0),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-                BottomNavigationBarItem(
-                  label: "",
-                  icon: Padding(
-                    padding: EdgeInsets.only(top: 5 * Scale.height),
-                    child: Column(
-                      children: [
-                        SvgPicture.asset(controller.currentIconUrl[MYPAGE]),
-                        SizedBox(height: 6 * Scale.height),
-                        Text(
-                          "마이페이지",
-                          style: textStyle(const Color(0xffcccccc),
-                              FontWeight.w400, "NotoSansKR", 12.0),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-              showSelectedLabels: false,
-              showUnselectedLabels: false,
-              selectedFontSize: 12 * Scale.width,
-              onTap: (index) {
-                homeController.onItemTapped(index);
-              },
-              currentIndex: _currentIndex,
-              type: BottomNavigationBarType.fixed,
-            );
-          }),
+      child: BottomNavigationBar(
+        backgroundColor: const Color(0xffffffff),
+        selectedLabelStyle: TextStyle(fontSize: 0),
+        unselectedLabelStyle: TextStyle(fontSize: 0),
+        items: [
+          BottomNavigationBarItem(
+            label: "",
+            icon: Padding(
+              padding: EdgeInsets.only(top: 5 * Scale.height),
+              child: Column(
+                children: [
+                  SvgPicture.asset(currentIconUrl[HOME]),
+                  SizedBox(height: 6 * Scale.height),
+                  Text(
+                    "홈",
+                    style: textStyle(const Color(0xffcccccc), FontWeight.w400,
+                        "NotoSansKR", 12.0),
+                  )
+                ],
+              ),
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: "",
+            icon: Padding(
+              padding: EdgeInsets.only(top: 5 * Scale.height),
+              child: Column(
+                children: [
+                  SvgPicture.asset(currentIconUrl[CATEGORY]),
+                  SizedBox(height: 6 * Scale.height),
+                  Text(
+                    "카테고리",
+                    style: textStyle(const Color(0xffcccccc), FontWeight.w400,
+                        "NotoSansKR", 12.0),
+                  )
+                ],
+              ),
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: "",
+            icon: Padding(
+              padding: EdgeInsets.only(top: 5 * Scale.height),
+              child: Column(
+                children: [
+                  SvgPicture.asset(currentIconUrl[LIKE]),
+                  SizedBox(height: 6 * Scale.height),
+                  Text(
+                    "찜한 상품",
+                    style: textStyle(const Color(0xffcccccc), FontWeight.w400,
+                        "NotoSansKR", 12.0),
+                  )
+                ],
+              ),
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: "",
+            icon: Padding(
+              padding: EdgeInsets.only(
+                top: 5 * Scale.height,
+              ),
+              child: Column(
+                children: [
+                  SvgPicture.asset(currentIconUrl[CHAT]),
+                  SizedBox(height: 6 * Scale.height),
+                  Text(
+                    "피드",
+                    style: textStyle(const Color(0xffcccccc), FontWeight.w400,
+                        "NotoSansKR", 12.0),
+                  )
+                ],
+              ),
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: "",
+            icon: Padding(
+              padding: EdgeInsets.only(top: 5 * Scale.height),
+              child: Column(
+                children: [
+                  SvgPicture.asset(currentIconUrl[MYPAGE]),
+                  SizedBox(height: 6 * Scale.height),
+                  Text(
+                    "마이페이지",
+                    style: textStyle(const Color(0xffcccccc), FontWeight.w400,
+                        "NotoSansKR", 12.0),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedFontSize: 12 * Scale.width,
+        onTap: (index) {
+          setState(() {
+            currentIconUrl[_currentIndex] = navigationIconUrl[_currentIndex];
+            _currentIndex = index;
+            currentIconUrl[_currentIndex] =
+                navigationOnTapIconUrl[_currentIndex];
+          });
+        },
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed,
+      ),
     );
   }
 
