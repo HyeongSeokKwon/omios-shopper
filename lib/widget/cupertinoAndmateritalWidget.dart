7001e5badc51453f79060e6036e349a821cb74f1
kwonhyeongseok <rnjsgudtjr@naver.com>
2022-07-07 17:23:46 +0900

    로그인 alertDialog 추가

--- lib/widget/cupertinoAndmateritalWidget.dart
+++ lib/widget/cupertinoAndmateritalWidget.dart
@@ -4,6 +4,8 @@ import 'package:cloth_collection/util/util.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../page/login/login.dart';
+
 Widget progressBar() {
   if (Platform.isIOS) {
     return Center(child: CupertinoActivityIndicator());
@@ -57,4 +59,72 @@ dynamic showAlertDialog(BuildContext context, String e) {
       },
     );
   }
+}
+
+dynamic loginAlertDialog(BuildContext context, Widget routePage) {
+  showDialog(
+      context: context,
+      builder: (context) {
+        if (Platform.isIOS) {
+          return CupertinoAlertDialog(
+            content: Text("로그인이 필요한 서비스입니다.",
+                style: textStyle(
+                    Colors.black, FontWeight.w500, 'NotoSansKR', 15.0)),
+            actions: <Widget>[
+              CupertinoDialogAction(
+                isDefaultAction: true,
+                child: Text("확인"),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => Login(
+                                routePage: routePage,
+                              )));
+                },
+              ),
+              CupertinoDialogAction(
+                isDefaultAction: true,
+                child: Text("취소"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        } else {
+          return AlertDialog(
+            content: Text("로그인이 필요한 서비스입니다.",
+                style: textStyle(
+                    Colors.black, FontWeight.w500, 'NotoSansKR', 15.0)),
+            actions: <Widget>[
+              TextButton(
+                child: Text(
+                  "확인",
+                  style: textStyle(
+                      Colors.black, FontWeight.w500, 'NotoSansKR', 15.0),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => Login(
+                                routePage: routePage,
+                              )));
+                },
+              ),
+              TextButton(
+                child: Text(
+                  "취소",
+                  style: textStyle(
+                      Colors.black, FontWeight.w500, 'NotoSansKR', 15.0),
+                ),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        }
+      });
 }
